cf2ea8cead6787915e0cbc852bbb8d7c2c662be8
Melekd8 <2049728@cegepoutaouais.qc.ca>
2025-10-03 13:47:11 -0400

    new ver 25-10-03

--- isaac plan de test 1.py
+++
@@ -1,5 +0,0 @@
-| Points de vies | Points d'attaques   | Points de défenses   |     Joueur 1       |    Joueur 2    | Résultat observé |  |
-|------------    |------------         |--------------------  |--------------------|
-|            |            |                    |                    |
-|            |            |                    |                    |
-|            |            |                    |                    |
